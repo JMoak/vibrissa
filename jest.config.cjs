e62ccd9393a233dae1d9bd380df5c95f5c5c2b02
Jordan Moak <jordan.moak@gmail.com>
2025-09-07 21:54:17 -0400

    chore(): add unit test configuration

---
+++ jest.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'mjs', 'cjs', 'json'],
+  transform: {
+    '^.+\\.(t|j)sx?$': ['@swc/jest'],
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  testMatch: ['**/?(*.)+(spec|test).(ts|tsx|js)'],
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: 'reports/junit',
+        outputName: 'junit.xml',
+        classNameTemplate: '{classname}',
+        titleTemplate: '{title}',
+        ancestorSeparator: ' › ',
+      },
+    ],
+  ],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+}
---
+++ jest.config.ts
@@ -0,0 +1,2 @@
+// Replaced by jest.config.cjs
+export {}
